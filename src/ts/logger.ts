01f0eb776f4b8a4a3ffa0ff2bccfd77a6bfb4598
SatoshiEmmerich <satoshi.emmerich@gmail.com>
2023-03-05 23:27:45 +0900

    add gcp logging

--- src/ts/logger.ts
+++ src/ts/logger.ts
@@ -1,9 +1,13 @@
 import moment from 'moment';
 import * as fs from 'fs/promises';
+import { Logging } from '@google-cloud/logging';
+
+// Creates a client
+const glog = new Logging({ projectId: 'tootback' }).log('tootback_batch');
 
 export function log(level: string, msg: string | object) {
+  const lvl = level.substring(0, 1).toUpperCase();
   if (process.env.TOOTBACK_IS_LOCAL) {
-    const lvl = level.substring(0, 1).toUpperCase();
     if (typeof msg == 'string') {
       msg.split(/[\r\n]+/g).forEach(l => fs.appendFile('./batch.log', `${moment().format()} [${lvl}] ${l}\n`));
     } else {
@@ -12,5 +16,25 @@ export function log(level: string, msg: string | object) {
         .split(/[\r\n]+/g)
         .forEach(l => fs.appendFile('./batch.log', `${moment().format()} [${lvl}] ${l}\n`));
     }
+  } else {
+    const sever = (l1: string) => {
+      switch (l1) {
+        case 'I':
+          return 'INFO';
+        case 'W':
+          return 'WARNING';
+        case 'E':
+          return 'ERROR';
+        default:
+          return 'INFO';
+      }
+    };
+    // The metadata associated with the entry
+    const metadata = {
+      resource: { type: 'global' },
+      // See: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+      severity: sever(lvl),
+    };
+    glog.write(glog.entry(metadata, msg));
   }
 }
