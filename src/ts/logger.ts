82efdceba91a4517290cefce5892a5499a5a02ed
SatoshiEmmerich <satoshi.emmerich@gmail.com>
2023-03-06 05:34:41 +0900

    logging

--- src/ts/logger.ts
+++ src/ts/logger.ts
@@ -2,7 +2,9 @@ import moment from 'moment';
 import * as fs from 'fs/promises';
 import { Logging } from '@google-cloud/logging';
 import { LogEntry } from '@google-cloud/logging/build/src/entry';
+import crypto from 'crypto';
 
+const rand = crypto.randomBytes(20).toString('hex').substring(0, 6);
 // Creates a client
 const glog = new Logging({ projectId: 'tootback' }).log('tootback_batch');
 
@@ -39,7 +41,7 @@ export function log(level: string, msg: string | object) {
       resource: { type: 'global' },
       // See: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
       severity: sever(lvl),
-      labels: { label: 'batchLog' },
+      labels: { runName: 'tootback-batch', runId: `tb:${rand}` },
     };
     return glog.write(glog.entry(metadata, msg));
   }
